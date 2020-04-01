faeb94878b8cbb251655a41752ac1336174478c8
Jessy Ayala <jessyayala@Jessys-iMac.local>
2020-03-31 19:28:05 -0700

    added files

---
+++ static/main.js
@@ -0,0 +1,33 @@
+// can be used to show details when hovering over pet objects
+// if so, include in base.html
+
+var hiddenClass = 'hidden';
+var shownClass = 'toggled-from-hidden';
+
+function petSectionHover() {
+    var children = this.children;
+    for(var i = 0; i < children.length; i++) {
+        var child = children[i];
+        if (child.className === hiddenClass) {
+            child.className = shownClass;
+        }
+    }
+}
+
+function petSectionEndHover() {
+    var children = this.children;
+    for(var i = 0; i < children.length; i++) {
+        var child = children[i];
+        if (child.className === shownClass) {
+            child.className = hiddenClass;
+        }
+    }
+}
+
+(function() {
+    var petSections = document.getElementsByClassName('petname');
+    for(var i = 0; i < petSections.length; i++) {
+        petSections[i].addEventListener('mouseover', petSectionHover);
+        petSections[i].addEventListener('mouseout', petSectionEndHover);
+    }
+}());
